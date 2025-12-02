70c5276a0299e6d033b6a9e08b694b6ad2b21455
Grinchuk Nadezda <grennad991@gmail.com>
2025-12-02 13:49:07 +0100

    fix(vite): set correct base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svrg from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "article_trainer",
+  base: "/article_trainer/",
   plugins: [react(), svrg()],
   css: {
     devSourcemap: true,
