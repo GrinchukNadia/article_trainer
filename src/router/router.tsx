7aabdfa6cfa3df5d0f343c5743173a5b50e0798e
Grinchuk Nadezda <grennad991@gmail.com>
2025-12-02 20:42:27 +0100

    ix(router): use basename for github pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,16 +7,21 @@ import TrainGrammar from "../features/TrainGrammar/TrainGrammar";
 import Train from "../features/train/TrainList/Train";
 // import CardBody from "../features/train/TrainWords/wordCards/CardBody";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { index: true, element: <Train /> },
+        { path: "train_words", element: <TrainWords /> },
+        { path: "impressum", element: <Impressum /> },
+        { path: "grammar", element: <TrainGrammar /> },
+        { path: "stats", element: <Stats /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      { index: true, element: <Train /> },
-      { path: "train_words", element: <TrainWords /> },
-      { path: "impressum", element: <Impressum /> },
-      { path: "grammar", element: <TrainGrammar /> },
-      { path: "stats", element: <Stats /> },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
