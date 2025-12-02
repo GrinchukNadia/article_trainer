689ea2a29110e3f52b809b9e4fb43d165a09beb7
Grinchuk Nadezda <grennad991@gmail.com>
2025-12-02 13:39:44 +0100

    vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svrg from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "article_trainer",
   plugins: [react(), svrg()],
   css: {
     devSourcemap: true,
