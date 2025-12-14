d081b3c208946f6cc20aa1588e5cd9f5ce8a2fb9
Grinchuk Nadezda <grennad991@gmail.com>
2025-12-14 16:21:31 +0100

    feat: enhance modal functionality and improve responsive styles

--- src/features/trainArticles/Train.tsx
+++ src/features/trainArticles/Train.tsx
@@ -3,6 +3,7 @@ import { modalMap } from "./trainModals/modals";
 import { useSearchParams } from "react-router-dom";
 import Modal from "./trainModals/Modal";
 import TilesGrid from "./trainTiles/TilesGrid";
+import { useEffect } from "react";
 
 const Train = () => {
   const [params, setParams] = useSearchParams();
@@ -12,13 +13,35 @@ const Train = () => {
     params.delete("modal");
     setParams(params, { replace: false });
   };
+
+  useEffect(() => {
+    if (!modalKey) return;
+
+    const originalOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    };
+  }, [modalKey]);
+
   return (
     <div className={styles.train}>
       <TilesGrid />
 
       {modalKey && modalKey in modalMap && (
-        <Modal variant={modalKey as "learnArticles" | "einEineTrainer" | "articleSprint" | "mistakeReview"  | "pluralTrainer" | "translateMaster"}  >
-          {modalMap[modalKey as keyof typeof modalMap] ({ close })}
+        <Modal
+          variant={
+            modalKey as
+              | "learnArticles"
+              | "einEineTrainer"
+              | "articleSprint"
+              | "mistakeReview"
+              | "pluralTrainer"
+              | "translateMaster"
+          }
+        >
+          {modalMap[modalKey as keyof typeof modalMap]({ close })}
         </Modal>
       )}
     </div>
